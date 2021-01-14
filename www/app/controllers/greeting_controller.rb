e71152a82e6f4ba8fa36f2827e2689d2158fe7ba
Shigeki Tanaka <dev@shigeki.tokyo>
2021-01-14 16:04:51 +0900

    json validate

--- www/app/controllers/greeting_controller.rb
+++ www/app/controllers/greeting_controller.rb
@@ -3,8 +3,16 @@ class GreetingController < ApplicationController
     expires_in 1.hours, public: true
     name = params[:name] || "Rails API"
 
-    render content_type: "application/hal+json",
-           json: { greeting: sprintf("Hello! %s.", name),
-                   _links: { self: { href: request.path } } }
+    body = { greeting: sprintf("Hello! %s.", name),
+             _links: { self: { href: request.path } } }
+
+    begin
+      JSON::Validator.validate!('schema/greeting.json', body)
+
+      render content_type: "application/hal+json", json: body
+    rescue => e
+      logger.error e
+      render content_type: "application/vnd.error+json", json: { message: e.message }
+    end
   end
 end
